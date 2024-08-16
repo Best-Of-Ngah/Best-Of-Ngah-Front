d522dbb6ef2e5ffbbe03d8a12eb4442616789946
junkaM <hei.mikanto.3@gmail.com>
2024-08-16 15:03:37 +0200

    add carousel template

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,11 @@
+import "../styles/carousel.css";
+
+const CarouselItem = ({ children, width }) => {
+  return (
+    <div className="carousel-item" style={{ width: width }}>
+      {children}
+    </div>
+  );
+};
+
+export default CarouselItem;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,40 @@
+import CarouselItem from "../../components/CarouselItem.jsx";
+import Carousel from "../../components/carousel.jsx";
+
+const Slide = ({ number }) => (
+  <div>
+    <img src={`https://via.placeholder.com/328x164.png?text=Slide ${number}`} />
+  </div>
+);
+
 const Home = () => {
-  return <div>This is Home page</div>;
+  return (
+    <div>
+      <Carousel>
+        <CarouselItem>
+          <Slide number={1} />
+        </CarouselItem>
+        <CarouselItem>
+          <Slide number={2} />
+        </CarouselItem>
+        <CarouselItem>
+          <Slide number={3} />
+        </CarouselItem>
+        <CarouselItem>
+          <Slide number={4} />
+        </CarouselItem>
+        <CarouselItem>
+          <Slide number={5} />
+        </CarouselItem>
+        <CarouselItem>
+          <Slide number={6} />
+        </CarouselItem>
+        <CarouselItem>
+          <Slide number={7} />
+        </CarouselItem>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Home;
