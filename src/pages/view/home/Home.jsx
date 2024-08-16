13dd09e7b2e9b7a283c91a30d8d972595cdefe68
Thony0666 <160373228+Anthonyo02@users.noreply.github.com>
2024-08-17 01:51:48 +0300

    feat: modal contact us

--- src/pages/view/home/Home.jsx
+++ src/pages/view/home/Home.jsx
@@ -1,14 +1,7 @@
 import { Grid } from "@mui/material";
 import Bannier from "../../../components/Bannier.jsx";
 import Descrit from "../../../components/Descrit.jsx";
-import Carousel from "../../../components/Carousel.jsx";
 import FinalProject from "../../../components/FinalProject.jsx";
-
-// const Slide = ({ number }) => (
-//   <div>
-//     <img src={`https://via.placeholder.com/328x164.png?text=Slide ${number}`} />
-//   </div>
-// );
 
 const Home = () => {
   return (
